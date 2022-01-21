e9981c03dc0e18f5fc271655f45e94d1826fde9c
Felicity Brammer <felicitybrammer@gmail.com>
2022-01-21 14:33:14 -0700

    added a seed file of db info

---
+++ server/seeds/pet-info.js
@@ -0,0 +1,492 @@
+{
+    name: "Brody",
+    description: "Very lumpy (cuz he’s so full of love)",
+    image:"",
+    category:"Dog",
+    location: "Vancouver",
+    answers: [
+      {
+        [ activity: "low", 
+          needs: "special food", 
+          household: "adults", 
+          age: "15", 
+          yard: "no", 
+          other-pets:  "cat and dog"]
+  
+        }
+    ]
+  },
+  {
+    name: "Keelah",
+    description: "Miami street dog. Speedy and snuggly.",
+    image:"",
+    category:"Dog",
+    location: "Vancouver",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adults", 
+          age: "14", 
+          yard: "no", 
+          other-pets:  "cat and dog"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Bishop",
+    description: "Teaching the baby his ways of getting taxes (snacks)... ",
+    image:"",
+    category:"Dog",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "baby", 
+          age: "10", 
+          yard: "yes", 
+          other-pets:  "no"]
+  
+        }
+    ]
+  },
+  {
+    name: "Frankie",
+    description: "French Bulldog Collie cross loves swimming and cuddles ",
+    image:"",
+    category:"Dog",
+    location: "Elgin, Scotland",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "3 children under 5", 
+          age: "3", 
+          yard: "yes", 
+          other-pets:  "no"]
+  
+        }
+    ]
+  },
+  {
+    name: "Shylah",
+    description: "Registered service dog. Husky cross. Very sweet yet also very shy. Likes to backtalk",
+    image:"",
+    category:"Dog",
+    location: "Nanaimo, BC",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "adults", 
+          age: "5", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  {
+    name: "Seren",
+    description: "Cockapooloves everything except cats!",
+    image:"",
+    category:"Dog",
+    location: "Pembroke, Wales",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "teens", 
+          age: "5", 
+          yard: "yes", 
+          other-pets:  "dogs only"]
+  
+        }
+    ]
+  },
+  {
+    name: "Paris",
+    description: "Cockapoo loves being a lap dog",
+    image:"",
+    category:"Dog",
+    location: "Pembroke, Wales",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "teens", 
+          age: "4", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  {
+    name: "Brother Cat",
+    description: "as wide eyed and sincere as his sister is sinister. Basically a yellow lab puppy in a snazzy ginger suit. So handsome. Kinda of a Himbo. Stands beside you while you're cooking and gently takes snacks off the counter with his paws.",
+    image:"",
+    category: "Cat",
+    location: "Victoria, BC",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adults", 
+          age: "4", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Sabine",
+    description: "the Goblin Queen. AKA Sharkberry. Sharpest kitten in existence. Insists on being domesticated. Has zero space boundaries and will steal the croissant right out of your hands. Unexpectedly affectionate, often while looking disgusted with you.",
+    category: "Cat",
+    location: "Victoria, BC",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adults", 
+          age: "1", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "NoName",
+    description: ""
+    category: "Dog",
+    location: "Nelson, BC",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adults", 
+          age: "8", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Marty",
+    description: " English Bulldog and likely Boxer mixed, 8 yrs old, deaf from birth, knows sign language, loves bum scratches, arm chair snuggles and destroying stuffies. Very food motivated.",
+    category: "Dog",
+    location: "Gibsons, BC",
+    answers: [
+      {
+        [ activity: "low", 
+          needs: "yes", 
+          household: "adults", 
+          age: "8", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Pippa",
+    description: "Pippa, English Bulldog,  was born with Spina Bifida, is incontinent, will be a diaper baby for life, very vocal when she wants attention (especially when she wants her diaper changed), loves to eat momma’s expensive shoes, runs very fast (especially after being caught eating momma’s expensive shoes).",
+    category: "Dog",
+    location: "Gibsons, BC",
+    answers: [
+      {
+        [ activity: "low", 
+          needs: "yes", 
+          household: "adults", 
+          age: "1", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "George",
+    description: "pug cross. He loves deep snow, cheese, and occupying as much of the sofa as he can. He is a sucker for a good chin scratch.",
+    category: "Dog",
+    location: "Ottawa",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "baby", 
+          age: "12", 
+          yard: "yes", 
+          other-pets:  "no"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Merrick",
+    description: "love people - especially young people! Merrick (Rottie) is super gentle and just wants butt scratches. Walks, tug play and good dog bones preferred!",
+    category: "Dog",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adult", 
+          age: "12", 
+          yard: "yes", 
+          other-pets:  "dogs"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Pip",
+    description: "love people - especially young people! Pip (Mini Schnauzer) is mostly a one person dog but will tolerate loving ‘smooches’. Walks, tug play and good dog bones preferred!",
+    category: "Dog",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adult", 
+          age: "12", 
+          yard: "yes", 
+          other-pets:  "dogs"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Koda",
+    description: "outgoing, loves bananas, hates the crate, perfect date would be chasing each other through the woods before sharing a swampy dip in the local pond",
+    category: "Dog",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "adult", 
+          age: "1", 
+          yard: "yes", 
+          other-pets:  "dogs"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Sam",
+    description: "Ex-sled dog. Alaskan husky. Very active and sweet. Loves to run and roll in snow. Submissive around other dogs. Unsure about children. A cheese stealer. ",
+    category: "Dog",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "adult", 
+          age: "8", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Mia",
+    description: "Sedentary senior who loves to sleep on heated blankets all day. Solar powered. ",
+    category: "Cat",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "16", 
+          yard: "no", 
+          other-pets:  "no other cats"]
+  
+        }
+    ]
+  },
+  
+  
+  
+  {
+    name: "Emma",
+    description: "Crested gecko. Loves to climb and leap into the abyss."
+    category: "Reptiles",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "unknown", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Sven",
+    description: "Sweet polydactyle ginger cat. Loves to sleep on shoulders",
+    category: "Cat",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adult", 
+          age: "3", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Douglas Fur",
+    description: "A barn rescue. Very food motivated",
+    category: "Cat",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "moderate", 
+          needs: "no", 
+          household: "adult", 
+          age: "1", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Jerome",
+    description: "Terrier rescue. Very sweet and shy. Hates cold weather",
+    category: "Dog",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "no", 
+          household: "adult", 
+          age: "2", 
+          yard: "yes", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Dick",
+    description: "Opinionated cockatiel. Loves to tear up cardboard and shriek. Will dance along to Dolly Parton.",
+    category: "Bird",
+    location: "Halifac",
+    answers: [
+      {
+        [ activity: "high", 
+          needs: "must be monitored", 
+          household: "adult", 
+          age: "unknown", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Andrew",
+    description: "Crested gecko. Loves to climb and leap into the abyss.",
+    category: "Reptiles",
+    location: "Whitehorse",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "unknown", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Gayle",
+    description: "Shy and quiet. Doesn't like to be caged.",
+    category: "Bird",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "unknown", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Frankie",
+    description: "Princess parrot who loves to sit on your head",
+    category: "Bird",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "2", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  {
+    name: "Rachel Maddow, Laurence O'Donnell, Joyanne Reid",
+    description: "Bonded rabbit trio",
+    category: "Other",
+    location: "Halifax",
+    answers: [
+      {
+        [ activity: "very low", 
+          needs: "no", 
+          household: "adult", 
+          age: "unknown", 
+          yard: "no", 
+          other-pets:  "yes"]
+  
+        }
+    ]
+  },
+  
+  
+  
