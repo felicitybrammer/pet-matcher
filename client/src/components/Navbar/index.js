786669b6ae3c3ca248c8405f10fab170c6bf7c5a
Michelle Nijjar <michellenijjar@hotmail.com>
2022-01-31 00:43:45 -0500

    styled navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 const Navbar = () => {
-    
+
     const logout = event => {
         event.preventDefault();
         Auth.logout();
-      };
-    
+    };
+
+      const [checked, setChecked] = useState(false);
+      const [radioValue, setRadioValue] = useState('1');
+
+
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
             <span role='img' aria-label='code'>
@@ -25,23 +30,63 @@ const Navbar = () => {
                 <nav className="text-center">
                     {Auth.loggedIn() ? (
                         <>
-                            <Link to="/profile">My Profile</Link>
+                            <ToggleButton
+                                type="radio"
+                                name="radio"
+                                value= "1"
+                                variant="outline-danger"
+                                checked={checked}
+                                onChange={(e) => setRadioValue(e.currentTarget.checked)}
+                            >
+                              <Link className = "link" to="/profile">Profile</Link>
+                            </ToggleButton>
+
                             {/* <Link to="/about">About</Link>
                             <Link to="/contact">Contact Us</Link> */}
-                            <a href="/" onClick={logout}>
-                                Logout
-                            </a>
-                        </>
-                    ) : (
-                        <>
-                            <Link to="/login">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </>
-                    )}
-                </nav>
-            </div>
+                    <ToggleButton
+                        type="radio"
+                        name="radio"
+                        value= "2"
+                        variant="outline-danger"
+                        checked={checked}
+                        onChange={(e) => setRadioValue(e.currentTarget.checked)}
+                        onClick={logout}
+                    >
+                        <Link className = "link" to="/logout">Logout</Link>
+                    </ToggleButton>
+                </>
+                ) : (
+                <>
+                    <ToggleButton
+                        type="radio"
+                        name="radio"
+                        value= "1"
+                        variant="outline-danger"
+                        checked= {checked}
+                        onChange={(e) => setRadioValue(e.currentTarget.checked)}
+                    >
+                      <Link className = "link" to="/login">Login</Link>
+                    </ToggleButton>
 
-        </header>
+                    <ToggleButton
+                        type="radio"
+                        name="radio"
+                        value= "2"
+                        variant="outline-danger"
+                        checked={checked}
+                        onChange={(e) => setRadioValue(e.currentTarget.checked)}
+                    >
+                        <Link className = "link" to="/signup">Sign Up</Link>
+                    </ToggleButton>
+                </>
+                    )}
+            </nav>
+   
+
+
+        </div>
+
+        </header >
     )
 }
 
