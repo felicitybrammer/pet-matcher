939091360b355fb00a120ab1009ad788ebe12444
Felicity Brammer <felicitybrammer@gmail.com>
2022-01-30 17:33:05 -0700

    feature/user-context-data

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Navbar = () => {
+    
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+      };
+    
     return (
         <header className="bg-secondary mb-4 py-2 flex-row align-center">
             <span role='img' aria-label='code'>
@@ -16,8 +23,21 @@ const Navbar = () => {
                 </Link>
 
                 <nav className="text-center">
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to="/profile">My Profile</Link>
+                            {/* <Link to="/about">About</Link>
+                            <Link to="/contact">Contact Us</Link> */}
+                            <a href="/" onClick={logout}>
+                                Logout
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </>
+                    )}
                 </nav>
             </div>
 
