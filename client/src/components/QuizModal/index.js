137f1a4d6358ba147aa9761eb0f25669e1da2410
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-01-31 15:20:56 -0500

    addQuiz mutations and quizmodal updated

--- client/src/components/QuizModal/index.js
+++ client/src/components/QuizModal/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_QUIZ } from '../../utils/mutations';
 
 const Quiz = () => {
+    const [addQuiz, {error}] = useMutation(ADD_QUIZ);
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        const userValue = event.target.value;
+        console.log(userValue)
+
+        // if (event.target.value = "female") {
+        //     console.log("female checked")
+        // }
+    }
 
     return (
 
         <div>
-            <form>
+            <form onSubmit= { handleFormSubmit }>
+
                 <p>Would you prefer a pet of a particular sex?</p>
-                <input type="checkbox" />
+                <input type="checkbox" value="female"/>
                 <label for="sex">Female</label>
-            <input type="checkbox" />
-            <label for="sex">Male</label>
-                    {/* <input type=“checkbox” />
-                    <label for=“sex”>Don’t Mind</label> */}
-            </form >
-            <form>
+                <input type="checkbox" value="male" />
+                <label for="sex">Male</label>
+            
                 <p>Would you prefer pet of a particular age?</p>
                     <input type="checkbox" />
                     <label for="age">Baby</label>
@@ -24,8 +36,7 @@ const Quiz = () => {
                     <label for="age">Adult</label>
                     <input type="checkbox" />
                     <label for="age">Senior</label>
-            </form >
-            <form>
+           
                 <p>Would you prefer a particular type of animal?</p>
                     <input type="checkbox" />
                     <label for="category">Dog</label>
@@ -35,8 +46,7 @@ const Quiz = () => {
                     <label for="category">Bird</label>
                     <input type="checkbox" />
                     <label for="category">Reptile</label>
-            </form>
-            <form>
+           
                 <p>How active would you prefer to be with your pet?</p>
                     <input type="checkbox" />
                     <label for="activity">Low</label>
@@ -44,15 +54,13 @@ const Quiz = () => {
                     <label for="activity">Moderate</label>
                     <input type="checkbox" />
                     <label for="activity">High</label>
-            </form>
-            <form>
+
                 <p>Would you consider a pet with special needs?</p>
-                    <input type="radio" name="need" value="yes" />
+                    <input type="radio" name="need" value="true" />
                     <label for="needs">Yes</label>
-                    <input type="radio" name="need" value="no" />
+                    <input type="radio" name="need" value="false" />
                     <label for="needs">No</label>
-            </form>
-            <form>
+            
                 <p>Do any of the following live in your household?</p>
                     <input type="checkbox" />
                     <label for="household">Baby</label>
@@ -62,12 +70,11 @@ const Quiz = () => {
                     <label for="household">Senior</label>
                     <input type="checkbox" />
                     <label for="household">Person with special needs</label>
-            </form>
-            <form>
+            
                 <p>Do any other pets live with you?</p>
-                    <input type="radio" name="pets" value="yes"/>
+                    <input type="radio" name="pets" value="true"/>
                     <label for="otherPets">Yes</label>
-                    <input type="radio" name="pets" value="no" />
+                    <input type="radio" name="pets" value="false" />
                     <label for="otherPets">No</label>
             </form>
             <button className="form-submit btn" type="submit">Submit</button>
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import{gql} from "@apollo/client";
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -22,4 +22,20 @@ export const ADD_USER = gql`
       }
     }
   }
+`;
+
+export const ADD_QUIZ = gql`
+  mutation addQuiz($sex: String, $age: String, $category: String, $activity: String, $needs: Boolean, $household: String, $otherPets: Boolean) {
+    addQuiz(sex: $sex, age: $age, category: $category, activity: $activity, needs: $needs, household: $household, otherPets: $otherPets) {
+    answers{
+      sex
+      age
+      category
+      activity
+      needs
+      household
+      otherPets
+    }
+  }
+}
 `;
