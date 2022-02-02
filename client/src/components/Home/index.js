627500fd0d24086032b01c983c3701881efbc7e0
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-02-02 02:26:21 -0500

    main page icons and buttons moved + warning colour

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,45 +1,57 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
 
     return (
+        <div>
+            <Carousel fade>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100"
+                        src={require("../../assets/images/funcat.jpg")}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item interval={5000}>
+                    <img
+                        className="d-block w-100"
+                        src={require("../../assets/images/whitedogkiss.jpg")}
+                        alt="Second slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={require("../../assets/images/gecko.jpg")}
+                        alt="Third slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
 
-        <Carousel fade>
-        <Carousel.Item interval={5000}>
-            <img
-                className="d-block w-100"
-                src={require("../../assets/images/funcat.jpg")}
-                alt="First slide"
-            />
-            <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-            <img
-                className="d-block w-100"
-                src={require("../../assets/images/whitedogkiss.jpg")}
-                alt="Second slide"
-            />
-            <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-                className="d-block w-100"
-                src={require("../../assets/images/gecko.jpg")}
-                alt="Third slide"
-            />
-            <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        </Carousel>
+            <div className="d-grid gap-2">
+                <Button variant="warning" size="lg">
+                    <strong>Join now to find your puurrrrrfect match!</strong>
+                </Button>
+            </div>
+
+
+        </div>
+
+
 
     )
 
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,10 @@ import { faCode } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import ToggleButton from 'react-bootstrap/ToggleButton';
-import Navbar from 'react-bootstrap/Navbar'
+import Navbar from 'react-bootstrap/Navbar';
+
+import okcLogo from '../../assets/images/clear8.jpg';
+
 const NavBar = () => {
 
     const logout = event => {
@@ -14,10 +17,12 @@ const NavBar = () => {
 
     const [checked, setChecked] = useState(false);
 
+    // style={{ display: 'flex', justifyContent: 'flex-end' }}
 
     return (
-        <Navbar expand="lg"  variant="dark" className="nav">
-            <div>
+        <Navbar expand="lg" variant="dark" className="nav" style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <img className="" src={okcLogo} alt="OkCreature's logo"></img>
                 {/* <span role='img' aria-label='code'>
                     {' '}
                     <FontAwesomeIcon icon={faCode} />
@@ -26,60 +31,65 @@ const NavBar = () => {
                     <h1 className="title">OkCreature</h1>
                 </Link>
             </div>
+
             <div className="navBtn">
-            {Auth.loggedIn() ? (
-                <>
-                    <ToggleButton
-                        type="radio"
-                        name="radio"
-                        value="1"
-                        variant="outline-danger"
-                        checked={checked}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
-                    >
-                        <Link className="link" to="/profile">Profile</Link>
-                    </ToggleButton>
+                {Auth.loggedIn() ? (
+                    <>
+                        <ToggleButton
+                            type="radio"
+                            name="radio"
+                            value="1"
+                            variant="warning"
+                            checked={checked}
+                            onChange={(e) => setChecked(e.currentTarget.checked)}
+                        >
+                            <Link className="link" to="/profile">Profile</Link>
+                        </ToggleButton>
 
-                    {/* <Link to="/about">About</Link>
+                        {/* <Link to="/about">About</Link>
                             <Link to="/contact">Contact Us</Link> */}
-                    <ToggleButton
-                        type="radio"
-                        name="radio"
-                        value="2"
-                        variant="outline-danger"
-                        checked={checked}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
-                        onClick={logout}
-                    >
-                        <Link className="link" to="/logout">Logout</Link>
-                    </ToggleButton>
-                </>
-            ) : (
-                <>
-                    <ToggleButton
-                        type="radio"
-                        name="radio"
-                        value="1"
-                        variant="outline-danger"
-                        checked={checked}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
-                    >
-                        <Link className="link" to="/login">Login</Link>
-                    </ToggleButton>
+                        <ToggleButton
+                            type="radio"
+                            name="radio"
+                            value="2"
+                            variant="warning"
 
-                    <ToggleButton
-                        type="radio"
-                        name="radio"
-                        value="2"
-                        variant="outline-danger"
-                        checked={checked}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
-                    >
-                        <Link className="link" to="/signup">Sign Up</Link>
-                    </ToggleButton>
-                </>
-            )}
-</div>
+                            checked={checked}
+                            onChange={(e) => setChecked(e.currentTarget.checked)}
+                            onClick={logout}
+                        >
+                            <Link className="link" to="/logout">Logout</Link>
+                        </ToggleButton>
+                    </>
+                ) : (
+                    <>
+                        <ToggleButton
+                            type="radio"
+                            name="radio"
+                            value="1"
+                            variant="outline-warning"
+                            checked={checked}
+                            onChange={(e) => setChecked(e.currentTarget.checked)}
+                        >
+                            <Link className="link" to="/login">Login</Link>
+                        </ToggleButton>
+                       
+
+                        <ToggleButton
+                            type="radio"
+                            name="radio"
+                            value="2"
+                            variant="outline-warning"
+                            checked={checked}
+                            onChange={(e) => setChecked(e.currentTarget.checked)}
+                        >
+                            <Link className="link" to="/signup">Sign Up</Link>
+                        </ToggleButton>
+                    </>
+                )}
+            </div>
+
+
         </Navbar>
 
     )
