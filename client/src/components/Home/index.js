60f8eca2e7979eb69769a778260ab3eb5ac8e76b
Ravneet Panglia <ravneet.panglia@gmail.com>
2022-02-02 22:47:54 -0500

    carousel images border added and same size

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -5,46 +5,46 @@ import Button from 'react-bootstrap/Button';
 const Home = () => {
 
     return (
-        <div>
+        <div className="carouselContainer">
             <Carousel fade>
                 <Carousel.Item interval={5000}>
-                    <img
+                    <img 
                         className="d-block w-100"
-                        src={require("../../assets/images/funcat.jpg")}
-                        alt="First slide"
+                        src={require("../../assets/images/funcat2.jpg")}
+                        alt="kid with cat"
                     />
                     <Carousel.Caption>
-                        <h3>OkCreature</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3>Every Creature Deserves A Loving Home</h3>
+                        <p>OkCreature is here to make that happen</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item interval={5000}>
                     <img
                         className="d-block w-100"
-                        src={require("../../assets/images/whitedogkiss.jpg")}
-                        alt="Second slide"
+                        src={require("../../assets/images/whitedogkiss2.jpg")}
+                        alt="happy dog owner getting kisses"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3>Not Sure What You're Looking For?</h3>
+                        <p>We've developed a pet-matching quiz to find the best fit for you and our creatures!</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
                         className="d-block w-100"
-                        src={require("../../assets/images/gecko.jpg")}
-                        alt="Third slide"
+                        src={require("../../assets/images/gecko2.jpg")}
+                        alt="bearded lizard in hand"
                     />
                     <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3>Match & Adopt Now!</h3>
+                        <p>In a mid-pandemic world, many pets are being returned to shelters. Let's work together to take care of thy creature.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
 
             <div className="d-grid gap-2">
                 <Button variant="warning" size="lg">
-                    <strong>Join now to find your puurrrrrfect match!</strong>
+                    <strong>Join OkCreature to find your puurrrrrfect match!</strong>
                 </Button>
             </div>
 
--- client/src/pages/Creators.js
+++ client/src/pages/Creators.js
@@ -7,7 +7,8 @@ import { Button } from "react-bootstrap";
 
 //Creator Images
 import ravImg from '../assets/team/rav2.jpg';
-
+import fbImg from '../assets/team/fb.png';
+import maksImg from '../assets/team/maks.jpg';
 
 const About = () => {
     return (
@@ -20,7 +21,7 @@ const About = () => {
 
             <div className="m-5" style={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Card style={{ width: '20rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img variant="top" src={fbImg} />
                     <Card.Body className="cardBody">
                         <Card.Title className="creatorTitle">Felicity Brammer</Card.Title>
                         <Card.Text style={{ fontSize: "10px" }}>
@@ -50,11 +51,11 @@ const About = () => {
 
             <div className="m-5" style={{ display: 'flex', justifyContent: 'space-around' }}>
                 <Card style={{ width: '20rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img variant="top" src={maksImg} />
                     <Card.Body className="cardBody">
                         <Card.Title className="creatorTitle">Makda Girmay</Card.Title>
                         <Card.Text style={{ fontSize: "10px" }}>
-                            Kinesiologist turned Web Developer here! I am on track to earn a Full-Stack Web Development certificate from the University of Toronto's School of Continuing Studies. I completed a Bachelor of Science in Human Kinetics and have experience working withinin Brain Injury rehab: specifically focused on return-to-work and improving quality of life treatment. With a passion for healthcare, I am in pursuit of furthering my skillset in a unique way by levering technology to one day improve lives. When I'm not in front of screens, I enjoy DIY-ing things around the house, gardening, exploring the world and making memories with friends and family.
+                        hi im Makda, i have a love hate relationship with code and a hate hate relationship with apollo. when im not in front of a screen i sleep and eat and have nightmares about code, but i also like to read anything but code documentation and watch movies but i guess thats another screen.
                         </Card.Text>
                         <div style={{ display: 'flex', justifyContent: 'space-around' }}>
                             <Button variant="warning" icon={faGithub} href="https://github.com/maks-pixel" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} /></Button>
