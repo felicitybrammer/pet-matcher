88bc4dc399922248a07556a8b1a56ea6c665f11d
Makda Girmay <makdagirmay4@gmail.com>
2022-02-02 22:09:13 -0500

    pet array

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,37 +1,72 @@
 import React from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery } from '@apollo/client';
 import { QUERY_ME } from "../utils/queries";
 import { QUERY_PETS } from "../utils/queries";
 
 const UserProfile = () => {
     const { loading, data } = useQuery(QUERY_ME);
-    const me = data?.me || [];
+    const { loading: petLoading, data: petData } = useQuery(QUERY_PETS)
+    const me = data?.me || {};
     console.log('profile:', me);
 
-    const { waiting, info } = useQuery(QUERY_PETS);
-    const pets = info?.pets || [];
-    console.log('Dashboard:', pets);
-    
-
+    const pets = petData?.pets || [];
+    console.log('profile:', pets);
+    // console.log(me.answers);
     //match answer key by mapping through pets
-    // const keyMatch = (me, pets) => {
-    //     pets.map((pet)=>{
-    //         const matchK = []
-    //         if(Object.keys(pet) === Object.keys(me.answers)){
-    //             [Object.keys(pet).value].push(matchK)
-    //             console.log(matchK)
-    //         } 
-    //     })
+    const allFilteredPets = []
+    if (pets, me) {
+        
+        pets.map((pet) => {
+
+            const filteredPet={}
+            Object.keys(pet).map((questionName) => {
+                const petAnswers = pet[questionName];
+                
+                
+                // console.log(petAnswers);
+                if (questionName === '_id') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'sex') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'age') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'category') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'activity') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'needs') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'household') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                } else if (questionName === 'otherPets') {
+                    console.log(petAnswers);
+                    filteredPet[questionName] = (petAnswers)
+                }
+
+            })
+console.log(filteredPet);
+allFilteredPets.push(filteredPet)
+        })
+        
+           
+    }
     //     //match the matched key values and add 1 for every same value
-    // }
-    // keyMatch();
+console.log(allFilteredPets);
+
     //any number over 9 add to an array
     //render pet cards by mapping through array 
     return (
         <div>
             <h1>Your Pet Matches!</h1>
             <div>
-              
+
             </div>
         </div>
     )
